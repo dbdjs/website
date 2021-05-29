48b0fc724d48ef8c59ef8ea907654974643077c7
Juan46xx <Juansantana75@icloud.com>
2021-05-29 17:52:53 -0400

    /old added but faster.

--- js/onload.js
+++ js/onload.js
@@ -34,7 +34,7 @@ startIntro();
 
 function startIntro() {
     const logo = document.createElement("img");
-    logo.src = "/images/DBDJSLOGOBLUEblackbackground.gif";
+    logo.src = "./images/DBDJSLOGOBLUEblackbackground.gif";
     logo.style.width = "100%";
     logo.style.maxWidth = "25vmin";
     logo.id = "logo"
@@ -44,7 +44,7 @@ function startIntro() {
             changeHeadTextAnimations()
         })
         logo.classList.add("fade")
-    }, 2000)
+    }, 1)
     };
 
 function changeHeadTextAnimations() {
@@ -55,11 +55,11 @@ function changeHeadTextAnimations() {
         logo.classList.remove("fade")
         setTimeout(function () {
             logo.classList.add("slide")
-        }, 1000)
+        }, 1)
     } else if (classes.includes("slide")) {
         setTimeout(function () {
             addRest()
-        }, 1000)
+        }, 1)
     }
 }
 
@@ -70,35 +70,8 @@ function addRest() {
     about.classList.add("box")
     about.classList.add("white")
     
-    const aboutHead = document.createElement("p")
-    aboutHead.classList.add("heading")
-    aboutHead.innerText = "About"
-    about.append(aboutHead)
-    const aboutDescription = document.createElement("p")
-    aboutDescription.innerText = "DBD.JS is a Package that was made for NodeJS Beginners, they are easy to maintain and use to build Discord Bots with ease";
-    about.append(aboutDescription)
-    about.onclick = function () {
-        window.location.href = "https://npmjs.com/package/dbd.js"
-    }
 
     flex.append(about)
-
-    const inspiration = document.createElement("div")
-    inspiration.classList.add("box")
-    inspiration.classList.add("white")
-    
-    const inspirationHead2 = document.createElement("p")
-    inspirationHead2.classList.add("heading")
-    inspirationHead2.innerText = "Inspiration"
-    inspiration.append(inspirationHead2)
-    const inspirationDescription = document.createElement("p")
-    inspirationDescription.innerText = "DBD.JS was inspired by a Software called Bot Designer for Discord, a good Software to build your bot easily.";
-    inspiration.append(inspirationDescription)
-    inspiration.onclick = function () {
-        window.location.href = "https://botdesignerdiscord.com"
-    }
-
-    flex.append(inspiration)
 
     body.append(flex)
 
@@ -111,7 +84,7 @@ function addRest() {
 
     const exampleText = document.createElement("p")
     exampleText.classList.add("heading")
-    exampleText.innerText = "Example"
+    exampleText.innerText = "DBD.JS v4"
 
     exampleCategory.append(exampleText)
     exampleFlex.append(exampleCategory)
@@ -123,99 +96,6 @@ function addRest() {
     const Pre = document.createElement("pre")
     Pre.classList.add("box")
     Pre.id = "code"
-
-    const example = document.createElement("code")
-
-    //Code Highlighting
-
-    //Requiring Package
-    example.append(create("newLine", ""))
-    example.append(create("declare", "const"))
-    example.append(create("space", " "))
-    example.append(create("used", "DBD"))
-    example.append(create("space", " "))
-    example.append(create("normal", "="))
-    example.append(create("space", " "))
-    example.append(create("function", "require"))
-    example.append(create("normal", "("))
-    example.append(create("string", "\"dbd.js\""))
-    example.append(create("normal", ");"))
-    
-    //Initialize Bot
-    example.append(document.createElement("br"))
-    example.append(create("newLine", ""))
-    example.append(create("declare", "const"))
-    example.append(create("space", " "))
-    example.append(create("used", "bot"))
-    example.append(create("space", " "))
-    example.append(create("normal", "="))
-    example.append(create("space", " "))
-    example.append(create("declare", "new"))
-    example.append(create("space", " "))
-    example.append(create("used", "DBD"))
-    example.append(create("normal", "."))
-    example.append(create("class", "Bot"))
-    example.append(create("normal", "({"))
-    example.append(document.createElement("br"))
-    example.append(create("newLine", ""))
-    example.append(create("tab", "  "))
-    example.append(create("notused", "token"))
-    example.append(create("normal", ":"))
-    example.append(create("space", " "))
-    example.append(create("string", "\"Discord Bot Token\""))
-    example.append(create("normal", ","))
-    example.append(document.createElement("br"))
-    example.append(create("newLine", ""))
-    example.append(create("tab", "  "))
-    example.append(create("notused", "prefix"))
-    example.append(create("normal", ":"))
-    example.append(create("space", " "))
-    example.append(create("string", "\"!\""))
-    example.append(document.createElement("br"))
-    example.append(create("newLine", ""))
-    example.append(create("normal", "})"))
-
-    // Bot.onMessage()
-    example.append(document.createElement("br"))
-    example.append(create("newLine", ""))
-    example.append(document.createElement("br"))
-    example.append(create("newLine", ""))
-    example.append(create("used", "bot"))
-    example.append(create("normal", "."))
-    example.append(create("function", "onMessage"))
-    example.append(create("normal", "()"))
-
-    // Bot.command({})
-    example.append(document.createElement("br"))
-    example.append(create("newLine", ""))
-    example.append(document.createElement("br"))
-    example.append(create("newLine", ""))
-    example.append(create("used", "bot"))
-    example.append(create("normal", "."))
-    example.append(create("function", "command"))
-    example.append(create("normal", "({"))
-    example.append(document.createElement("br"))
-    example.append(create("newLine", ""))
-    example.append(create("tab", "  "))
-    example.append(create("notused", "name"))
-    example.append(create("normal", ":"))
-    example.append(create("space", " "))
-    example.append(create("string", "\"ping\""))
-    example.append(create("normal", ","))
-    example.append(document.createElement("br"))
-    example.append(create("newLine", ""))
-    example.append(create("tab", "  "))
-    example.append(create("notused", "code"))
-    example.append(create("normal", ":"))
-    example.append(create("space", " "))
-    example.append(create("string", "`Pong! $botPing MS`"))
-    example.append(document.createElement("br"))
-    example.append(create("newLine", ""))
-    example.append(create("normal", "})"))
-
-    Pre.append(example)
-    exampleCode.append(Pre)
-    body.append(exampleCode)
 
    function create(className, text) {
        const element = document.createElement("span")
